bce0c9d844611b58cc1b8cab7ce3518357a820b6
ArinjayBhola <arinjay26bhola@gmail.com>
2026-02-08 14:35:22 +0530

    schema change

--- db/schema.ts
+++ db/schema.ts
@@ -70,28 +70,6 @@ export const accounts = pgTable(
   })
 );
 
-export const sessions = pgTable("session", {
-  sessionToken: text("sessionToken").primaryKey(),
-  userId: text("userId")
-    .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  expires: timestamp("expires", { mode: "date" }).notNull(),
-});
-
-export const verificationTokens = pgTable(
-  "verificationToken",
-  {
-    identifier: text("identifier").notNull(),
-    token: text("token").notNull(),
-    expires: timestamp("expires", { mode: "date" }).notNull(),
-  },
-  (verificationToken) => ({
-    compositePk: primaryKey({
-      columns: [verificationToken.identifier, verificationToken.token],
-    }),
-  })
-);
-
 // --- Outreach Schema ---
 export const outreach = pgTable("outreach", {
   id: uuid("id").defaultRandom().primaryKey(),
